f2af102af892773ee015d7201b4f9c861baffc94
Mec-iS <tunedconsulting@gmail.com>
2019-04-26 14:54:21 +0100

    Add very basic benchmarking

--- run-wasm-with-python.py
+++ run-wasm-with-python.py
@@ -1,13 +1,63 @@
+import timeit
+
 from wasmer import Instance
 
-path = 'target/wasm32-unknown-unknown/release/medium-xp.wasm'
+path = 'target/wasm32-unknown-unknown/release/rust_wasm_python_101.wasm'
+
+def add(a, b):
+    return a + b
+
+def fibonacci(n):
+    i = 0
+    nextterm = 1
+    present = 1
+    previous = 0
+
+    while i < n:
+        nextterm = present + previous
+        present = previous
+        previous = nextterm
+        i = i + 1
+
+    return nextterm
+
+def string_loop(string):
+    for s in string:
+        s = s * 10
 
 with open(path, 'rb') as bytecode:
     wasm_bytes = bytecode.read()
     instance = Instance(wasm_bytes)
-    result = instance.exports.simple_add(12, 12)
 
-    print('Modules exported from Rust: ')
-    print(instance.exports)  # this will print function's name
-    print('call simple_add(12, 12): ')
-    print(result)  # 24
+    # print exported functions
+    print("Modules exported from Rust: ")
+    print(instance.exports)
+
+    # assign functions
+    simple_add = instance.exports.simple_add
+    fibo = instance.exports.fibo
+    loop_str = instance.exports.loop_str
+
+    # try a simple addition
+    result = simple_add(12, 12)
+    print("call simple_add(12, 12): ")
+    print(result)
+
+    # some timing using popular functions
+    t_py = timeit.timeit('(add(n-1, n) for n in range(100, 1000))', number=10000)
+    print('py add', t_py)
+
+    t_wasm = timeit.timeit('(simple_add(n-1, n) for n in range(100, 1000))', number=10000)
+    print(t_wasm)
+
+    t_py = timeit.timeit('(fibonacci(n) for n in range(100, 1000))', number=10000)
+    print('py fibo', t_py)
+
+    t_wasm = timeit.timeit('(fibo(n) for n in range(100, 1000))', number=10000)
+    print(t_wasm)
+
+    t_py = timeit.timeit('(string_loop(n) for n in range(100, 1000))', number=10000)
+    print('py str loop', t_py)
+
+    t_wasm = timeit.timeit('(loop_str(n) for n in range(100, 1000))', number=10000)
+    print(t_wasm)
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,32 @@
 #[no_mangle]
 pub extern fn simple_add(a: i32, b: i32) -> i32 { a + b}
+
+
+#[no_mangle]
+pub extern fn fibo(n: i32) -> i32 {
+    if n < 0 {
+        panic!("{} is negative!", n);
+    } else if n == 0 {
+        panic!("zero is not a right argument to fibonacci()!");
+    } else if n == 1 {
+        return 1;
+    }
+
+    let mut sum = 0;
+    let mut last = 0;
+    let mut curr = 1;
+    for _i in 1..n {
+        sum = last + curr;
+        last = curr;
+        curr = sum;
+    }
+    sum
+}
+
+#[no_mangle]
+pub extern fn loop_str(string: String) -> () {
+    // for strings types in Rust see http://www.suspectsemantics.com/blog/2016/03/27/string-types-in-rust/
+    for c in string.chars() {
+        let _s = c.to_digit(10).unwrap() * 10;
+    }
+}
