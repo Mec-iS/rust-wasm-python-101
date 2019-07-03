988e9711576e04cbb54a331f9f72d5cffc567cdb
Mec-iS <tunedconsulting@gmail.com>
2019-07-03 22:47:03 +0100

    Make timeit to work (results in secs)
        py add 1.269273363001048
        t_wasm add 8.957978017999267
        py fibo 43.752940606000266
        t_wasm fibo 1.2281139650003752
        py shapely convex hull 48.14261505699869
        py shapely convex hull 50.62281900100061
        t_wasm rust-geo convex hull 2.3167089839989785

--- run-wasm-with-python.py
+++ run-wasm-with-python.py
@@ -6,6 +6,12 @@ from timing.src_py import *
 
 path = 'target/wasm32-unknown-unknown/release/rust_wasm_python_101.wasm'
 
+context = '''from wasmer import Instance
+path = 'target/wasm32-unknown-unknown/release/rust_wasm_python_101.wasm'
+wasm_bytes = open(path, 'rb').read()
+instance = Instance(wasm_bytes)
+'''
+
 def run_test():
     with open(path, 'rb') as bytecode:
         wasm_bytes = bytecode.read()
@@ -26,29 +32,29 @@ def run_test():
         print("call simple_add(12, 12): ")
         print(result)
 
-        # some timing using popular functions
-        t_py = timeit.timeit('(py_simple_add(n-1, n) for n in range(100, 1000))', number=10000)
+        #some timing using popular functions
+        t_py = timeit.timeit('[py_simple_add(n-1, n) for n in range(100, 1000)]', number=10000, setup='from timing.src_py import py_simple_add')
         print('py add', t_py)
 
-        t_wasm = timeit.timeit('(simple_add(n-1, n) for n in range(100, 1000))', number=10000)
+        t_wasm = timeit.timeit('[simple_add(n-1, n) for n in range(100, 1000)]', number=10000, setup=context+'simple_add = instance.exports.simple_add')
         print('t_wasm add', t_wasm)
 
-        t_py = timeit.timeit('(py_fibonacci(n) for n in range(100, 1000))', number=10000)
+        t_py = timeit.timeit('[py_fibonacci(n) for n in range(100, 1000)]', number=1000, setup='from timing.src_py import py_fibonacci')
         print('py fibo', t_py)
 
-        t_wasm = timeit.timeit('(fibonacci(n) for n in range(100, 1000))', number=10000)
+        t_wasm = timeit.timeit('[fibo(n) for n in range(100, 1000)]', number=1000, setup=context+'fibo = instance.exports.fibo')
         print('t_wasm fibo', t_wasm)
 
-        t_py = timeit.timeit('(py_string_loop(str(n)*100) for n in range(100, 1000))', number=10000)
-        print('py str loop', t_py)
+        # t_py = timeit.timeit('[py_string_loop(str(n)*100) for n in range(100, 1000)]', number=1000, setup='from timing.src_py import py_string_loop')
+        # print('py str loop', t_py)
+        #
+        # t_wasm = timeit.timeit('[loop_str(str(n)*100) for n in range(100, 1000)]', number=1000, setup=context+'loop_str = instance.exports.loop_str')
+        # print('t_wasm str loop', t_wasm)
 
-        t_wasm = timeit.timeit('(loop_str(str(n)*100) for n in range(100, 1000))', number=10000)
-        print('t_wasm str loop', t_wasm)
-
-        t_py = timeit.timeit('(py_shapely_convex_hull() for n in range(100, 1000))', number=10000)
+        t_py = timeit.timeit('[py_shapely_convex_hull() for n in range(100, 1000)]', number=1000, setup='from timing.src_py import py_shapely_convex_hull')
         print('py shapely convex hull', t_py)
 
-        t_wasm = timeit.timeit('(rust_geo_convex_hull() for n in range(100, 1000))', number=10000)
+        t_wasm = timeit.timeit('[rust_geo_convex_hull() for n in range(100, 1000)]', number=1000, setup=context+'rust_geo_convex_hull = instance.exports.rust_geo_convex_hull')
         print('t_wasm rust-geo convex hull', t_wasm)
 
 if __name__ == '__main__':
